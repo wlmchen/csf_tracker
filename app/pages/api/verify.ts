5e5ba6fae5e5e37e4747f3fe1927f35c50c2ccc3
wlmchen <65417452+wlmchen@users.noreply.github.com>
2023-08-04 22:44:24 -0700

    Allow unauthed users to verify

--- app/pages/api/verify.ts
+++ app/pages/api/verify.ts
@@ -8,12 +8,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const session = await getServerSession(req, res, authOptions);
-
-  if (!session) {
-    res.status(401).json({ message: "You must be logged in." });
-    return;
-  }
 
   if (req.method !== "GET") return res.status(400).json({ message: "Oops" });
 
