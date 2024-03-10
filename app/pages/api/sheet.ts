6017598481a1cf5dd1ae4b461751218cd10226f7
Kaival Shah <kaival.s.shah@gmail.com>
2024-03-10 12:11:32 -0700

    clean

--- app/pages/api/sheet.ts
+++ app/pages/api/sheet.ts
@@ -6,12 +6,12 @@ dotenv.config();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        // Allow requests from any origin (you might want to restrict this in production)
+
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-        // Respond to preflight requests
+
         if (req.method === 'OPTIONS') {
             return res.status(200).end();
         }
@@ -28,9 +28,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             const gsapi = google.sheets({ version: 'v4', auth: client });
 
             const opt = {
-                // spreadsheetId: '1Le5gEylvcypo8OsHeUiQ_6MPzrmjBJKB5lBhsx3WYRQ',
                 spreadsheetId: '1jcno4HUH7TKqvVGPYOmyPoc7OFu6jlSXCn3UjZSuaqY',
-                // range: 'Sheet1!A2:A'
                 range: 'Volunteer Opps!B16:F'
             };
 
